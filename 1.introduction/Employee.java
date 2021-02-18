e5069d3325300a609983eeb3b800de68138755c8
akana <mao.akana@gmail.com>
2021-02-18 21:00:54 +0700

    create Employee

---
+++ 1.introduction/Employee.java
@@ -0,0 +1,32 @@
+public class Employee {
+  private String firstname;
+  private String lastname;
+  private int yearOfBirth;
+  private double salary;
+  private String job;
+
+  public Employee(String lastname, String firstname, int yearOfBirth, double salary, String job) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.yearOfBirth = yearOfBirth;
+    this.salary = salary;
+    this.job = job;
+  }
+
+  public String getFullName() {
+    return this.firstname + " " + this.lastname;
+  }
+
+  public void setJob(String job) {
+    this.job = job;
+  }
+
+  public int getAge() {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    Employee jame = new Employee("bond", "jame", 1970, 0.0, "Actor");
+    System.out.println(jame.getFullName() + " " + jame.getAge());
+  }
+}
